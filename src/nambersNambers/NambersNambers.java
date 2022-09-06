54c849f75c01c6ec213fcd49bb5c98fa4f68d489
MEMORA\augusto.cerqueira <piresaugusto046@gmail.com>
2022-09-06 18:06:47 -0300

    x

--- src/nambersNambers/NambersNambers.java
+++ src/nambersNambers/NambersNambers.java
@@ -1,31 +1,33 @@
 package nambersNambers;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class NambersNambers {
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
-        float c = 0;
+        float c = 1;
         float tp = 0;
         float ti = 0;
         float ac = 0;
         System.out.println("Você que contar quantos numeros");
         float v = x.nextFloat();
-        do {
-            System.out.println("Digite um numero");
+        while (c <= v) {
+            System.out.println("digite um numero");
             float a = x.nextFloat();
             if (a % 2 == 0) {
-            tp++;
+                tp++;
             }
             if (a % 2 != 0) {
-            ti++;
+                ti++;
             }
-            if (a < 100) {
-            ac++;
+            if (a > 100) {
+                ac++;
             }
-            float m = a;
-            float M = m + a;
             c++;
-        }while (c == v);
+        }
+        System.out.println("Total de pares " + tp);
+        System.out.println("Total de impares " + ti);
+        System.out.println("Total acima de cem " + ac);
     }
 }
