a33490bc234b6593d845057375455e223e793251
MEMORA\augusto.cerqueira <piresaugusto046@gmail.com>
2022-10-03 16:48:30 -0300

    snclnlndl

--- src/canetasDiverssas/Caneta.java
+++ src/canetasDiverssas/Caneta.java
@@ -1,30 +1,49 @@
 package canetasDiverssas;
 
+import java.io.FilterOutputStream;
+import java.util.Scanner;
+
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
+    public String modelo;
+    public String cor;
+    public float ponta;
+    private int carga = 100;
+   private boolean tampada;
     void status() {
+        String tp = "destampada";
+        if (tampada == true) {
+            tp = "tampada";
+        }
         System.out.println("Uma caneta " + this.cor);
-        System.out.println("Esta tampada? " + this.tampada);
+        System.out.println("Esta tampada? " + tp);
         System.out.println("O modelo e " + this.modelo);
         System.out.println("A ponta e " + this.ponta);
         System.out.println("Esta com " + this.carga + "% de carga");
     }
-    void  rabiscar() {
-        if (tampada == true) {
-            System.out.println("ERRO");
+    void escrever() {
+        Scanner sc = new Scanner(System.in);
+        if (this.tampada == true) {
+            System.out.println("A caneta ta tampada");
         }
         else {
-            System.out.println("Rabisco");
+            System.out.println("Pode escrever");
+            String texto = sc.nextLine();
+            System.out.println(texto);
+        }
+    }
+    void  rabiscar() {
+        if (this.tampada == true) {
+            System.out.println("A caneta ta tampada");
+        }
+        else {
+            System.out.println("Rabiscando");
+            System.out.println("//--__//__--|||||////_______--------");
         }
     }
     void tampar() {
-        tampada = true;
+        this.tampada = true;
     }
     void destampar() {
-        tampada = false;
+        this.tampada = false;
     }
 }
--- src/canetasDiverssas/CriadorDeCanetas.java
+++ src/canetasDiverssas/CriadorDeCanetas.java
@@ -4,25 +4,41 @@ import java.util.Scanner;
 
 public class CriadorDeCanetas {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Quantas caneta você quer fazer?");
-        int quantc = sc.nextInt();
-        String[] model = new String[quantc];
-        String[] corr = new String[quantc];
-        float[] pont = new float[quantc];
-        int[] carg = new int[quantc];
-        for (int c = 0; c < quantc; c++) {
-            System.out.printf("Qual o modelo da %d° caneta \n" ,c + 1);
-            String M = sc.nextLine();
-            model[c] = M;
-            System.out.printf("Qual a cor da %d° caneta \n" ,c + 1);
-            String C = sc.nextLine();
-            corr[c] = C;
-            System.out.printf("Qual o tipo de ponta da %d° cameta \n" ,c + 1);
-            float P = sc.nextFloat();
-            pont[c] = P;
-            System.out.printf("Quanto de carga a caneta %D° tem" ,c + 1);
+        Caneta c1 = new Caneta();
+        Caneta c2 = new Caneta();
+        Caneta c3 = new Caneta();
+        Caneta c4 = new Caneta();
 
-        }
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.modelo = "bic esperogrfica";
+        c1.destampar();
+
+        c2.cor = "Preta";
+        c2.ponta = 1f;
+        c2.modelo = "bic esperogrfica";
+        c2.destampar();
+
+        c3.cor = "Verde";
+        c3.ponta = 1f;
+        c3.modelo = "bic esperogrfica";
+        c3.tampar();
+
+        c4.cor = "Vermelho";
+        c4.ponta = 1f;
+        c4.modelo = "bic esperogrfica";
+        c4.tampar();
+
+        c1.status();
+        System.out.println("");
+        c2.status();
+        System.out.println("");
+        c3.status();
+        System.out.println("");
+        c4.status();
+        System.out.println("");
+
+        c2.escrever();
+        c1.rabiscar();
     }
 }
