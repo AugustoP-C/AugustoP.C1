4243351898ab19ba5bab2423e48ebc865a3f6920
MEMORA\augusto.cerqueira <piresaugusto046@gmail.com>
2022-10-05 14:36:19 -0300

    projeto gaguinho java

---
+++ src/Gagagaguinho.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+import java.util.Scanner;
+
+public class Gagagaguinho {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("|-----Qual-seu-nome-?----|");
+        String nome = sc.nextLine();
+        String nomeCapitalizado = capitaliza(nome);
+        System.out.println(nomeCapitalizado);
+    }
+
+    private static String capitaliza(String nome) {
+        nome = nome.toLowerCase();
+        String[] nomes = nome.split(" ");
+        int n = nomes.length;
+        int n2 = nomes.length;
+        String nomeret = "";
+        for (int c = 0; c < n; c++){
+
+                nomeret += " " + nomes[c] + " " + nomes[c] + " " + nomes[c] + " " + nomes[c];
+
+
+        }
+        return nomeret;
+    }
+}
+
