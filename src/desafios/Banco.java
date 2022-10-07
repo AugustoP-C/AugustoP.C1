c3dc1cea2fe536fe8be9969458ab70b27f03e596
MEMORA\augusto.cerqueira <piresaugusto046@gmail.com>
2022-10-07 17:25:31 -0300

    desafios da conta bancaria

--- src/desafios/Banco.java
+++ src/desafios/Banco.java
@@ -2,19 +2,6 @@ package desafios;
 
 public class Banco {
     public static void main(String[] args) {
-        ContaBancaria c1 = new ContaBancaria();
-        ContaBancaria c2 = new ContaBancaria();
 
-        c1.setDono("jubileu");
-        c1.maisSaldo();
-        c1.setTipo("Poupanssa");
-        c1.setNumConta(377);
-        c1.status();
-
-        c2.setDono("claudete");
-        c2.maisSaldo();
-        c2.setTipo("Corrente");
-        c2.setNumConta(139);
-        c2.status();
     }
 }
--- src/desafios/ContaBancaria.java
+++ src/desafios/ContaBancaria.java
@@ -1,68 +1,119 @@
 package desafios;
 
-import java.awt.*;
 import java.util.Scanner;
 
 public class ContaBancaria {
-    private int numConta;
-    private String tipo;
+    // atribuidores
+    public int numConta;
+    protected String tipo;
     private String dono;
-     private float saldo;
-     public void maisSaldo() {
-         Scanner sc = new Scanner(System.in);
-         System.out.println("qual o valor a depositar");
-         float dep = sc.nextFloat();
-         this.saldo += dep;
-     }
-     public void menosSaldo() {
-         Scanner sc = new Scanner(System.in);
-         System.out.println("Antes de retira digite o numero de conta");
-         int co = sc.nextInt();
-         if (co == this.numConta) {
-             System.out.println("qual o valor a ser retirado");
-             float dep = sc.nextFloat();
-             this.saldo -= dep;
-         }
-         else {
-             System.out.println("descupe mas vc errou o numero");
-         }
-     }
-     public void status() {
-         Scanner sc = new Scanner(System.in);
-         System.out.println("Antes de exibir os estatus digite o numero de sua conta");
-         int co = sc.nextInt();
-         if (co == this.numConta) {
-             System.out.println("o numero dessa conta e " + this.numConta);
-             System.out.println("o tipo da conta e " + this.tipo);
-             System.out.println("o dono da conta e " + this.dono);
-             System.out.println("o saldo da conta e de " + this.saldo);
-         }
-         else {
-             System.out.println("descupe mas vc errou o numero");
-         }
-     }
+    private float saldo;
+    private boolean status;
+    //metodos especiasi
+    public ContaBancaria(float sal,boolean con) {
+        this.saldo = sal;
+        this.status = false;
+    }
+    //metodos comum
+    public void abrirConta(String tip,boolean stat) {
+        setTipo(tip);
+        setStatus(stat);
+        if (tip == "cc") {
+            this.saldo = 50;
+        }
+        if (tip =="cp") {
+            this.saldo = 150;
+        }
+    }
+    public void fecharConta() {
+        if (this.saldo > 0) {
+            System.out.println("Essa conta ainda tem dinhairo");
+        }
+        if (this.saldo < 0) {
+            System.out.println("Você ainda ta devendo");
+        }
+        setStatus(false);
+    }
+    public void depositar(float dep) {
+        if (this.status == true) {
+            setSaldo(getSaldo() + dep);
+        }
+        else {
+            System.out.println("Não pode depositar con a conta fachada");
+        }
+    }
+    public void sacar(float sac) {
+        if (this.status == true) {
+            if (this.saldo > sac) {
+                this.saldo -= sac;
+            }
+            else {
+                System.out.println("Saldo insuficiente");
+            }
+        }
+        else {
+            System.out.println("Não pode sacar pois a conta esta sem dinheiro");
+        }
+    }
+    public void pagarMensal() {
+        float mens = 0;
+        if (this.tipo.equals("cc")) {
+            mens = 12;
+        }
+        if (this.tipo.equals("cp")) {
+            mens = 20;
+        }
+        if (this.status == true) {
+            if (this.saldo > mens) {
+                this.saldo -= mens;
+            }
+            else {
+                System.out.println("Saldo insuficiente");
+            }
+        }
+        else {
+            System.out.println("Ipossivel pagar com a conta fachada");
+        }
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean stat) {
+        this.status = stat;
+    }
+
+    //metodos geters e seters
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float sal) {
+        this.saldo = sal;
+    }
 
     public String getDono() {
         return dono;
     }
 
-    public void setDono(String dono) {
-        this.dono = dono;
+    public void setDono(String don) {
+        this.dono = don;
     }
 
     public String getTipo() {
         return tipo;
     }
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
+    public void setTipo(String tip) {
+        this.tipo = tip;
     }
 
     public int getNumConta() {
         return numConta;
     }
 
-    public void setNumConta(int numConta) {
-        this.numConta = numConta;
+    public void setNumConta(int numC) {
+        this.numConta = numC;
     }
 }
