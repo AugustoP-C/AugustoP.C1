b65f05f23fa1ab6a121e84239acd6a7d4092281c
MEMORA\augusto.cerqueira <piresaugusto046@gmail.com>
2022-10-11 17:47:24 -0300

    projetos do jassa

---
+++ src/jastaProject/bubbleSort.java
@@ -0,0 +1,11 @@
+package jastaProject;
+
+import java.util.Scanner;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quanto numero vc quer digitar?");
+        int nc = sc.nextInt();
+    }
+}
