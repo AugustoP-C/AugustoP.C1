698f7fc68d09b9cf5a19b4bb631a7fe53cae63ec
MEMORA\augusto.cerqueira <piresaugusto046@gmail.com>
2022-10-11 17:47:04 -0300

    desafios da conta bancaria

--- src/desafios/ContaBancaria.java
+++ src/desafios/ContaBancaria.java
@@ -1,7 +1,5 @@
 package desafios;
 
-import java.util.Scanner;
-
 public class ContaBancaria {
     // atribuidores
     public int numConta;
@@ -10,62 +8,75 @@ public class ContaBancaria {
     private float saldo;
     private boolean status;
     //metodos especiasi
-    public ContaBancaria(float sal,boolean con) {
-        this.saldo = sal;
-        this.status = false;
+    public ContaBancaria() {
+        this.setSaldo(0);
+        this.setStatus(false);
     }
     //metodos comum
-    public void abrirConta(String tip,boolean stat) {
-        setTipo(tip);
-        setStatus(stat);
-        if (tip == "cc") {
-            this.saldo = 50;
+    public void statosDaConta() {
+        System.out.println("Conta: " + this.getNumConta());
+        System.out.println("Tipo: " + this.getTipo());
+        System.out.println("Dono: " + this.getDono());
+        System.out.println("Saldo: " + this.getSaldo());
+        System.out.println("Status: " + this.getStatus());
+    }
+    public void abrirConta(String tip) {
+        this.setTipo(tip);
+        int nc = (int) (5 + Math.random() * (100 - 5)) + 100 + (int) (5 + Math.random() * (100 - 5));
+        this.setNumConta(nc);
+        this.setStatus(true);
+        if (tip.equals("cc")) {
+            this.setSaldo(50f);
         }
-        if (tip =="cp") {
-            this.saldo = 150;
+        if (tip.equals("cp")) {
+            this.setSaldo(150f);
         }
+        System.out.println("conta aberta com sucesso");
     }
     public void fecharConta() {
-        if (this.saldo > 0) {
+        if (this.getSaldo() > 0) {
             System.out.println("Essa conta ainda tem dinhairo");
         }
-        if (this.saldo < 0) {
+        if (this.getSaldo() < 0) {
             System.out.println("Você ainda ta devendo");
         }
-        setStatus(false);
+        this.setStatus(false);
+        System.out.println("conta fechada com sucesso");
     }
     public void depositar(float dep) {
-        if (this.status == true) {
-            setSaldo(getSaldo() + dep);
+        if (this.getStatus()) {
+            this.setSaldo(this.getSaldo() + dep);
+            System.out.println("deposito realisado na conta de " + this.getDono());
         }
         else {
             System.out.println("Não pode depositar con a conta fachada");
         }
     }
     public void sacar(float sac) {
-        if (this.status == true) {
-            if (this.saldo > sac) {
-                this.saldo -= sac;
+        if (this.getStatus()) {
+            if (this.getSaldo() >= sac) {
+                this.setSaldo(this.getSaldo() - sac);
+                System.out.println("Saque realizado na conta de " + this.getDono());
             }
             else {
                 System.out.println("Saldo insuficiente");
             }
         }
         else {
-            System.out.println("Não pode sacar pois a conta esta sem dinheiro");
+            System.out.println("Não pode sacar pois a conta esta fechada");
         }
     }
     public void pagarMensal() {
         float mens = 0;
-        if (this.tipo.equals("cc")) {
+        if (this.getTipo().equals("cc")) {
             mens = 12;
-        }
-        if (this.tipo.equals("cp")) {
+        } else if (this.getTipo().equals("cp")) {
             mens = 20;
         }
-        if (this.status == true) {
-            if (this.saldo > mens) {
-                this.saldo -= mens;
+        if (this.getStatus()) {
+            if (this.getSaldo() > mens) {
+                this.setSaldo(this.getSaldo() - mens);
+                System.out.println("Pagamento feito com sucesso");
             }
             else {
                 System.out.println("Saldo insuficiente");
@@ -75,16 +86,15 @@ public class ContaBancaria {
             System.out.println("Ipossivel pagar com a conta fachada");
         }
     }
-
-    public boolean isStatus() {
+    //metodos geter e seters
+    public boolean getStatus() {
         return status;
     }
 
-    public void setStatus(boolean stat) {
-        this.status = stat;
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 
-    //metodos geters e seters
     public float getSaldo() {
         return saldo;
     }
