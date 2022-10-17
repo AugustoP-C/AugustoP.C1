83c4f7101294153079a1e068eb75f3d8c8c77709
MEMORA\augusto.cerqueira <piresaugusto046@gmail.com>
2022-10-17 16:39:03 -0300

    projetos do jassa

--- src/jastaProject/bubbleSort.java
+++ src/jastaProject/bubbleSort.java
@@ -1,15 +1,21 @@
 package jastaProject;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class bubbleSort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Quanto numero vc quer digitar?");
-        int nc = sc.nextInt();
-            for (int c1 = 0; c1 < nc; c1++) {
+        int[] nums = new int[sc.nextInt()];
+            for (int c1 = 0; c1 < nums.length; c1++) {
                 System.out.printf("digite o %d° numero \n", c1 + 1);
-
+                int nx = sc.nextInt();
+                nums[c1] = nx;
             }
+        Arrays.sort(nums);
+            for (int con: nums) {
+                System.out.println(con);
+        }
     }
 }
