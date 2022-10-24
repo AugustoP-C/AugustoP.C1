e84e0c712e13473ffb57bf576d877ae2fca16320
MEMORA\augusto.cerqueira <piresaugusto046@gmail.com>
2022-10-24 18:02:48 -0300

    aqba matematica

--- src/abaMatematica/MatematicaBasica.java
+++ src/abaMatematica/MatematicaBasica.java
@@ -1,10 +1,15 @@
 package abaMatematica;
 
+import java.util.Random;
+
 public class MatematicaBasica {
     public static void main(String[] args) {
         double ale = Math.random();
-        int n = (int) (5 + ale * (30 - 5));
-        System.out.println(n);
-
+        //int n = (int) (5 + ale * (20 - 5));
+        //System.out.println(n);
+        System.out.println(ale);
+        /*Random le = new Random();
+        int a = le.nextInt(0,3);
+        System.out.println(a);*/
     }
 }
