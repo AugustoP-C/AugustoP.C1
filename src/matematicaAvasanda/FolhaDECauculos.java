61e6ec09b70086edef267549e431e4e86313a54e
MEMORA\augusto.cerqueira <piresaugusto046@gmail.com>
2022-12-13 16:06:07 -0300

    projeto matematica

--- src/matematicaAvasanda/FolhaDECauculos.java
+++ src/matematicaAvasanda/FolhaDECauculos.java
@@ -2,6 +2,12 @@ package matematicaAvasanda;
 
 public class FolhaDECauculos {
     public static void main(String[] args) {
+        Equacao2Gral bas = new Equacao2Gral();
+        Funcao2Gral fun = new Funcao2Gral();
 
+        bas.Baskara(4f, 4f, 0f);
+        System.out.println(fun.PontosCartesiano(4f, 4f, 0f));
+        System.out.println("");
+        fun.Indicasoes(4f, 4f, 0f);
     }
 }
